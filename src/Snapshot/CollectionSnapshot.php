9cbe12328bd0285ae377119f4aa10b290a7f7d49
Baptiste Clavié <clavie.b@gmail.com>
2014-07-18 01:21:06 +0200

    Add PropertyAccess component in the CollectionSnapshot
    [reset_minion]

--- src/Snapshot/CollectionSnapshot.php
+++ src/Snapshot/CollectionSnapshot.php
@@ -15,6 +15,8 @@ use Traversable,
     ArrayAccess,
     InvalidArgumentException;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
 use Totem\AbstractSnapshot;
 
 /**
@@ -34,18 +36,18 @@ class CollectionSnapshot extends AbstractSnapshot
      * Construct the snapshot
      *
      * @param mixed $data Either an array or a traversable, data to take a snapshot of
-     * @param mxied $key  Key to use as a primary key
+     * @param mixed $pkey Key to use as a primary key
      *
      * @throws InvalidArgumentException if the $data is not an array or a Traversable
      * @throws InvalidArgumentException if the $data is not an at least 2 dimensional array
      * @throws InvalidArgumentException if one of the arrays of the collection does not have a $key key
-     *
-     * @todo use ArrayObject instead of plain arrays ?
      */
-    public function __construct($data, $key)
+    public function __construct($data, $pkey)
     {
         $this->data = [];
         $this->raw  = $data;
+
+        $accessor = PropertyAccess::createPropertyAccessor();
 
         if ($data instanceof Traversable) {
             $data = iterator_to_array($data);
@@ -61,19 +63,11 @@ class CollectionSnapshot extends AbstractSnapshot
                 throw new InvalidArgumentException('The given array / Traversable is not a collection as it contains non numeric keys');
             }
 
-            if ($value instanceof Traversable) {
-                $value = iterator_to_array($value);
+            if (!$accessor->isReadable($value, $pkey)) {
+                throw new InvalidArgumentException(sprintf('The key "%s" is not defined or readable in one of the elements of the collection', $pkey));
             }
 
-            if (!is_array($value) && !$value instanceof ArrayAccess) {
-                throw new InvalidArgumentException(sprintf('One of the elements of the collection is not an array, got "%s" instead', is_object($value) ? get_class($value) : gettype($value)));
-            }
-
-            if (!isset($value[$key])) {
-                throw new InvalidArgumentException(sprintf('The key "%s" is not defined of the elements of the collection', $key));
-            }
-
-            $this->data[$value[$key]] = $value;
+            $this->data[$accessor->getValue($value, $pkey)] = $value;
         }
 
         parent::normalize();
